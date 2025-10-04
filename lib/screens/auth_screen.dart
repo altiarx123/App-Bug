dece1da941ea63be9dd9858e1e04f9496356cebe
Luciferdevil013 <sujeetyadav8004100@gmail.com>
2025-10-04 22:08:16 +0530

    Fix Issue

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,12 +1,24 @@
+import 'package:flutter/material.dart';
 import 'phone_auth_screen.dart';
 
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
 
-TextButton(
-onPressed: () {
-Navigator.push(
-context,
-MaterialPageRoute(builder: (_) => const PhoneAuthScreen()),
-);
-},
-child: const Text("Login with Phone Number"),
-),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Auth Screen')),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const PhoneAuthScreen()),
+            );
+          },
+          child: const Text("Login with Phone Number"),
+        ),
+      ),
+    );
+  }
+}
